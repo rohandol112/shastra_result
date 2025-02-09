e514cd191ca136f19c253ff7b7ef32f58bb641c5
Chirayugiri <chirayugiri149@gmail.com>
2025-02-09 21:36:19 +0530

    complete

---
+++ backend/cleaner.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import os
+import numpy as np
+
+def clean_csv(df):
+    
+    # Remove '@' from the HackerRank ID column
+    df["HackerRank ID"] = df["HackerRank ID"].astype(str).str.lstrip("@")
+    df = df.replace(np.nan, "") 
+    return df
+
+
+if __name__ == '__main__':
+    clean_csv()
